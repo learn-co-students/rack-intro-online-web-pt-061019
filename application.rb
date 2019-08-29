ba0e16136e5ddcab36ced0dade65560a63d7037f
Khris Punzalan <khrispunzalan@gmail.com>
2019-08-28 20:53:48 -0700

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Khris"
     resp.finish
   end
 
 end
 
+# return a response which consists of the status code, 
+# any headers, and the body.
